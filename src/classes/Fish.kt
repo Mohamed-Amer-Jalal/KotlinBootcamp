848423125cec74c56335af6216fb454c5420fc0c
Mohamed Amer <mohamedamerjalal@gmail.com>
2024-09-22 22:56:45 +0300

    Initial commit

--- src/classes/Fish.kt
+++ src/classes/Fish.kt
@@ -6,13 +6,11 @@ fun main() {
 }
 
 class Fish(val friendly: Boolean = true, volumeNeeded: Int) {
-    var size: Int = if (friendly) {  volumeNeeded } else { volumeNeeded *2 }
+    val size: Int = if (friendly) {  volumeNeeded } else { volumeNeeded *2 }
 
     init { println("first init block") }
 
     constructor(): this(true, 9) { println("running secondary constructor") }
-
-    init { size = if (friendly) {  volumeNeeded } else { volumeNeeded *2 } }
 
     init { println("constructed fish of size $volumeNeeded final size ${this.size}") }
 
