30773649c7054c34a7b073adbcff07fe000c7fb2
M-Amer <mohamedamerjalal@gmail.com>
2024-10-05 20:37:19 +0300

    trickOrTreat up

--- src/compose/LambdaExpressions.kt
+++ src/compose/LambdaExpressions.kt
@@ -13,9 +13,7 @@ fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
     if (isTrick) {
         return trick
     } else {
-        if (extraTreat != null) {
-            println(extraTreat(5))
-        }
+        if (extraTreat != null) println(extraTreat(5))
         return treat
     }
 }
